215d71e9d61f91310b4aa71d5c06d432894faba6
NathanNdjoni <brakiplace@gmail.com>
2019-05-31 15:16:38 +0200

    Exo3+Exo5 fini crée un h1 avec un paragraph + un Ul avec de Li

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Exo4 
+ReactDOM.render(  <div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
+
+//Exo 5 
+ReactDOM.render(  <div><ul>Hello world!</ul><li>1</li><li>2</li><li>3</li></div>, document.getElementById('root2'));
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+/* 
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul> 
+*/
